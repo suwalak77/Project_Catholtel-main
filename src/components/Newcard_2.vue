d10825b0f26832f7de8584c137139ebdc9f5a501
suwalak77 <141695813+suwalak77@users.noreply.github.com>
2023-10-16 13:11:25 +0700

    Update Newcard_2.vue

--- src/components/Newcard_2.vue
+++ src/components/Newcard_2.vue
@@ -1,11 +1,12 @@
 <template>
     <div class="box_all">
-    <div class="col" v-for="(n) in travelList" :key="n">
+    <div class="col" v-for="(n,i) in travelList" :key="n">
       <div class="card" style="width: 60rem;" >
         <img :src="n.img" class="card-img-top">
         <div class="card-body">
           <h3 class="card-title">{{ n.name }}</h3>
-          
+          <a class="btn btn-primary" @click="list_local_control(i.name, i.quantity)"> จองห้องพัก </a>
+        
 
         </div>
       </div>
@@ -21,9 +22,9 @@
 import { ref } from 'vue';
 
 const travelList=ref([
-    {name:' ห้องพัก:ขนาดเล็ก/ราคา:400',   img:"https://www.chillpainai.com/src/wewakeup/scoop/img_scoop/scoop/jajah_student/cat%20hotel/cat6_1.jpg"},
-    {name:' ห้องพัก:ขนาดใหญ่/ราคา:700',   img:"https://www.chillpainai.com/src/wewakeup/scoop/img_scoop/scoop/jajah_student/cat%20hotel/cat8_1.jpg"},
-    {name:' ห้องพัก:แบบรวม/ราคา:300',   img:"https://www.chillpainai.com/src/wewakeup/scoop/img_scoop/scoop/jajah_student/cat%20hotel/cat1_1.jpg"},])
+    {name:' ห้องพัก:ขนาดเล็ก/ราคา:400',   img:"https://www.chillpainai.com/src/wewakeup/scoop/img_scoop/scoop/jajah_student/cat%20hotel/cat6_1.jpg", quantity: 0},
+    {name:' ห้องพัก:ขนาดใหญ่/ราคา:700',   img:"https://www.chillpainai.com/src/wewakeup/scoop/img_scoop/scoop/jajah_student/cat%20hotel/cat8_1.jpg", quantity: 0},
+    {name:' ห้องพัก:แบบรวม/ราคา:300',   img:"https://www.chillpainai.com/src/wewakeup/scoop/img_scoop/scoop/jajah_student/cat%20hotel/cat1_1.jpg", quantity: 0},])
 
 </script>
 
@@ -46,7 +47,7 @@ border-radius: 60px;
 
 .card{
 justify-content: center;
-margin: 100px;
+margin-right: 100px;
 margin-top: 50px;
 text-align: center;
 }
